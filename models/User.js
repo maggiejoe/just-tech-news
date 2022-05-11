4474765db259ac74d67ed97f8cf2e0908e910abd
maggiejoe <maggiejoseph94@gmail.com>
2022-05-11 14:19:27 -0400

    added password security features

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init(
@@ -58,6 +63,7 @@ User.init(
               return updatedUserData;
             }
           },
+
         // TABLE CONFIGURATION OPTIONS GO HERE (http://sequelize.org/v5/manual/models-definition.html#configuration))
 
         // pass in our imported sequelize connection (the direct connection to our database)
